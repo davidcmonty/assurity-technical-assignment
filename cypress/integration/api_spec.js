24f732e9b26d5ce8887e9bafeb24955dd8413135
David Montgomery <davidcmonty@gmail.com>
2018-04-22 09:37:14 +1200

    Added project to repo

---
+++ cypress/integration/api_spec.js
@@ -0,0 +1,55 @@
+//
+// **** Assurity Technical Assignment  ****
+//
+// Acceptance Criteria:
+// 1. Name = "Carbon credits"
+// 2. CanRelist = true
+// 3. The Promotions element with Name = "Gallery" has a Description that contains the text "2x larger image"
+
+describe('Assurity Technical Assignment', () => {
+  const getItems = () =>
+    cy.request('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+    .its('body')
+
+  it('Returns JSON', () => {
+    cy.request('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+  })
+
+  it('Returns 200 response code', function () {
+    cy.request('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+      .its('status')
+      .should('equal', 200)
+  })
+
+  it('Assert 1: Element "Name" is equal to "Carbon Credits"', function () {
+    cy.request('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+
+      .should(function (response) {
+        expect(response.body).to.have.property('Name', 'Carbon credits')
+      })
+  })
+
+  it('Assert 2: Element "CanRelist" is equal to "true"', function () {
+    cy.request('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+      .should(function (response) {
+        expect(response.body).to.have.property('CanRelist', true)
+      })
+  })
+
+  it('Assert 3: Promotions element with Name "Gallery", has a Description that contains the text "2x larger image"', function () {
+    const getPromo = () => getItems().its('Promotions')
+
+    getPromo().each(function($element, index) {
+      for(var key in $element) {
+        if ($element.hasOwnProperty(key)) {
+          if ($element[key] == 'Gallery') {
+            expect($element).to.have.property('Description').contains('2x larger image')
+          }
+        }
+      }
+    })
+  })
+})

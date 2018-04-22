22cb0b53fccaba1d4d43d219e07854c5f3eb32a5
David Montgomery <davidcmonty@gmail.com>
2018-04-22 12:36:33 +1200

    Minor code refactor

--- cypress/integration/api_spec.js
+++ cypress/integration/api_spec.js
@@ -9,37 +9,37 @@
 describe('Assurity Technical Assignment', () => {
   const api = 'https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false'
 
-  it('Successfully returns JSON', () => {
+  it('Returns JSON', () => {
     cy.request(api)
       .its('headers')
       .its('content-type')
       .should('include', 'application/json')
   })
 
-  it('Successfully returns 200 response code', function () {
+  it('Returns 200 response code', function () {
     cy.request(api)
       .its('status')
       .should('equal', 200)
   })
 
-  it('Assert 1: Element "Name" is equal to "Carbon Credits"', function () {
+  it('1: Element "Name" is equal to "Carbon Credits"', function () {
     cy.request(api)
       .should(function (response) {
         expect(response.body).to.have.property('Name', 'Carbon credits')
       })
   })
 
-  it('Assert 2: Element "CanRelist" is equal to "true"', function () {
+  it('2: Element "CanRelist" is equal to "true"', function () {
     cy.request(api)
       .should(function (response) {
         expect(response.body).to.have.property('CanRelist', true)
       })
   })
 
-  it('Assert 3: Promotions element with Name "Gallery", has a Description that contains the text "2x larger image"', function () {
+  it('3: Promotions element with Name "Gallery", has a Description that contains the text "2x larger image"', function () {
     const getPromo = () => cy.request(api).its('body').its('Promotions')
 
-    getPromo().each(function($element, index) {
+    getPromo().each(function($element) {
       for(var key in $element) {
         if ($element.hasOwnProperty(key)) {
           if ($element[key] == 'Gallery') {
